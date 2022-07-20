6e76769a9946de239237979f2c47e6f359dbc637
harshit-106 <109383805+harshit-106@users.noreply.github.com>
2022-07-20 23:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Who is the father of Computers?",
+        a: "James Gosling",
+        b: "Charles Babbage",
+        c: "Dennis Ritchie",
+        d: "Bjarne Stroustrup",
+        correct: "b",
+    },
+    {
+        question: "Who is the President of US?",
+        a: "Narendra Modi",
+        b: "John biden",
+        c: "Manmohan Singh",
+        d: "Rahul Gandhi",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "Jason Object Notation",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "Which of the following is correct about JavaScript?",
+a: "JavaScript is an Object-Based language",
+b: "JavaScript is Assembly-language",
+c: "JavaScript is an Object-Oriented language",
+d: "JavaScript is a High-level language",
+        correct: "a",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
